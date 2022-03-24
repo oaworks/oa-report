635eea9be927656426370527d2651cff9f5dee0a
sssoz <ouchsophy@gmail.com>
2022-03-24 15:30:55 +0000

    Load Tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -12,12 +12,21 @@ const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
 module.exports = function(eleventyConfig) {
   eleventyConfig.setLibrary('md', markdownLib)
 
+  // Watch tailwindCSS
+  eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
+  eleventyConfig.addWatchTarget('./styles/tailwind.css')
+
   // Set directories to pass through to the dist folder
   eleventyConfig.addPassthroughCopy('./src/img/');
   eleventyConfig.addPassthroughCopy('./src/media/');
   eleventyConfig.addPassthroughCopy('./src/fonts/');
   eleventyConfig.addPassthroughCopy('./src/js/');
   eleventyConfig.addPassthroughCopy('./src/favicons/');
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+ 
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  })
 
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true
---
+++ styles/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['dist/**/*.html'],
+  safelist: [],
+  theme: {
+    extend: {
+      colors: {
+        change: 'black',
+      },
+    },
+  },
+  plugins: [],
+}
