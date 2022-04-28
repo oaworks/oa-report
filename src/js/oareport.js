35da314aaa9b023ea01f3077bed981f24b2f545f
sssoz <ouchsophy@gmail.com>
2022-04-28 16:13:17 +0100

    Remove champs for now

--- src/js/oareport.js
+++ src/js/oareport.js
@@ -1,15 +1,16 @@
 const base = 'https://beta.oa.works/report/';
 
 oareport = function(org) {
-  const articles = axios.get(base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22"),
+  let articles = axios.get(base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22"),
   oaArticles = axios.get(base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:true"),
-  canArchive = axios.get(base + "articles?sort=published:desc&q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:%20false%20AND%20can_archive:true"),
-  archivingChamps = axios.get(base + "articles/terms/author_names?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20%20AND%20has_repository_copy:%22true%22%20AND%20oa_status:%22green%22");
-  const art = base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22";
-  console.log("org: " + org);
-  console.log("articles: " + art);
+  canArchive = axios.get(base + "articles?sort=published:desc&q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:%20false%20AND%20can_archive:true");
 
-  axios.all([articles, oaArticles, canArchive, archivingChamps])
+  console.log("org: " + org);
+  console.log("articles: " + base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22");
+  console.log("oa_articles: " + base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:true");
+  console.log("can_archive: " + base + "articles?sort=published:desc&q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:%20false%20AND%20can_archive:true");
+
+  axios.all([articles, oaArticles, canArchive])
   .then(axios.spread((...responses) => {
     let articlesContents = document.querySelector("#articles"),
     oaArticlesContents = document.querySelector("#articles_oa"),
@@ -17,9 +18,7 @@ oareport = function(org) {
     canArchiveContents = document.querySelector("#can_archive"),
     canArchiveOaPercentageContents = document.querySelector("#can_archive_percent_oa"),
     canArchiveLatestContents = document.querySelector("#can_archive_latest"),
-    canArchiveLatestJournalContents = document.querySelector("#can_archive_latest_journal"),
-    archivingChampsContents = document.querySelector("#archiving_champs"),
-    archivingChampsTopContents = document.querySelector("#archiving_champs_top");
+    canArchiveLatestJournalContents = document.querySelector("#can_archive_latest_journal");
 
     const articles = responses[0].data,
     oaArticles = responses[1].data,
@@ -28,9 +27,7 @@ oareport = function(org) {
     canArchiveAll = responses[2].data.hits.hits,
     canArchiveOaPercentage = ((((oaArticles+canArchive))/articles)*100).toFixed(2),
     canArchiveLatest = responses[2].data.hits.hits[0]._source.title,
-    canArchiveLatestJournal = responses[2].data.hits.hits[0]._source.journal,
-    archivingChampsLength = responses[3].data.length,
-    archivingChampsTop = responses[3].data[0].term;
+    canArchiveLatestJournal = responses[2].data.hits.hits[0]._source.journal;
 
     articlesContents.innerHTML = articles;
     oaArticlesContents.innerHTML = oaArticles;
@@ -39,8 +36,6 @@ oareport = function(org) {
     canArchiveOaPercentageContents.innerHTML = canArchiveOaPercentage;
     canArchiveLatestContents.innerHTML = canArchiveLatest;
     canArchiveLatestJournalContents.innerHTML = canArchiveLatestJournal;
-    archivingChampsContents.innerHTML = archivingChampsLength;
-    archivingChampsTopContents.innerHTML = archivingChampsTop;
 
   })).catch(error => console.error(error));
 };
