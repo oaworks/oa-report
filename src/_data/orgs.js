0137986f288ce457a20b7fab0c624f24b1aef6a6
sssoz <ouchsophy@gmail.com>
2022-05-03 15:52:33 +0100

    Reduce build to 2500

--- src/_data/orgs.js
+++ src/_data/orgs.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 module.exports = async () => {
-  const { data } = await axios.get("https://beta.oa.works/report/orgs?q=*&excludes=aliases,fundref,sheets,acronyms,paid,analysis,strategy,policy&size=5000");
+  const { data } = await axios.get("https://beta.oa.works/report/orgs?q=*&excludes=aliases,fundref,sheets,acronyms,paid,analysis,strategy,policy&size=2500");
 
   return data;
 };
