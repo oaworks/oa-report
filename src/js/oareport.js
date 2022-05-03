6ef4b8228299ad9fb959bf748693765403a8f30a
sssoz <ouchsophy@gmail.com>
2022-05-03 23:14:14 +0100

    Clean up queries

--- src/js/oareport.js
+++ src/js/oareport.js
@@ -1,43 +1,82 @@
 const base = 'https://beta.oa.works/report/';
+let isPaper, isOA, complianceRate, canDepositAAM, hasPolicy, policyURL;
+
+// Detect browser’s locale to display human-readable numbers
+getUsersLocale = function() {
+  return navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator.language;
+};
 
 oareport = function(org) {
-  let articles = axios.get(base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22"),
-  oaArticles = axios.get(base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:true"),
-  canArchive = axios.get(base + "articles?sort=published:desc&q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:%20false%20AND%20can_archive:true");
+  let report = base + "orgs?q=name:%22" + org + "%22";
 
   console.log("org: " + org);
-  console.log("articles: " + base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22");
-  console.log("oa_articles: " + base + "articles/count?q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:true");
-  console.log("can_archive: " + base + "articles?sort=published:desc&q=author_affiliations:%22" + org + "%22%20OR%20funder_names:%22" + org + "%22%20AND%20is_oa:%20false%20AND%20can_archive:true");
+  console.log("report: " + base + "orgs?q=name:%22" + org + "%22");
 
-  axios.all([articles, oaArticles, canArchive])
-  .then(axios.spread((...responses) => {
-    let articlesContents = document.querySelector("#articles"),
-    oaArticlesContents = document.querySelector("#articles_oa"),
-    oaPercentageContents = document.querySelector("#percent_oa"),
-    canArchiveContents = document.querySelector("#can_archive"),
-    canArchiveOaPercentageContents = document.querySelector("#can_archive_percent_oa"),
-    canArchiveLatestContents = document.querySelector("#can_archive_latest"),
-    canArchiveLatestJournalContents = document.querySelector("#can_archive_latest_journal");
+  axios.get(report).then(response => {
+    let countQueryBase = base + "articles/count?";
 
-    const articles = responses[0].data,
-    oaArticles = responses[1].data,
-    oaPercentage = ((oaArticles/articles)*100).toFixed(2),
-    canArchive = responses[2].data.hits.total,
-    canArchiveAll = responses[2].data.hits.hits,
-    canArchiveOaPercentage = ((((oaArticles+canArchive))/articles)*100).toFixed(2),
-    canArchiveLatest = responses[2].data.hits.hits[0]._source.title,
-    canArchiveLatestJournal = responses[2].data.hits.hits[0]._source.journal;
+    isPaper        = axios.get(countQueryBase + response.data.hits.hits[0]._source.analysis.is_paper);
+    isOA           = axios.get(countQueryBase + response.data.hits.hits[0]._source.analysis.is_oa);
+    complianceRate = axios.get(countQueryBase + response.data.hits.hits[0]._source.analysis.compliance);
+    canDepositAAM  = axios.get(countQueryBase + response.data.hits.hits[0]._source.strategy.email_author_aam.query);
+    // hasPolicy      = axios.get(response.data.hits.hits[0]._source.policy.supported_policy);
+    //
+    // if (hasPolicy === true) {
+    //   policyURL = response.data.hits.hits[0]._source.policy.url;
+    // }
 
-    articlesContents.textContent = articles;
-    oaArticlesContents.textContent = oaArticles;
-    oaPercentageContents.textContent = oaPercentage;
-    canArchiveContents.textContent = canArchive;
-    canArchiveOaPercentageContents.textContent = canArchiveOaPercentage;
-    canArchiveLatestContents.textContent = canArchiveLatest;
-    canArchiveLatestJournalContents.textContent = canArchiveLatestJournal;
+    Promise.all([isPaper, isOA, complianceRate, canDepositAAM])
+      .then(function (results) {
+        let isPaper = results[0].data,
+            isOA    = results[1].data,
+            canDepositAAM = results[3].data;
 
-  })).catch(error => console.error(error));
+        let articlesContents = document.querySelector("#articles"),
+            oaArticlesContents = document.querySelector("#articles_oa"),
+            oaPercentageContents = document.querySelector("#percent_oa"),
+            canArchiveContents = document.querySelector("#can_archive"),
+            canArchiveOaPercentageContents = document.querySelector("#can_archive_percent_oa"),
+            canArchiveLatestContents = document.querySelector("#can_archive_latest"),
+            canArchiveLatestJournalContents = document.querySelector("#can_archive_latest_journal");
+
+        articlesContents.textContent = isPaper.toLocaleString(getUsersLocale());
+        oaArticlesContents.textContent = isOA.toLocaleString(getUsersLocale());
+        oaPercentageContents.textContent = ((isOA/isPaper)*100).toFixed(2);
+        canArchiveContents.textContent = canDepositAAM.toLocaleString(getUsersLocale());
+        canArchiveOaPercentageContents.textContent = ((((isOA+canDepositAAM))/isPaper)*100).toFixed(2);
+      }
+    ).catch(error => console.error("ERROR: " + error));
+  })
+  .catch(error => console.error("ERROR: " + error));
+
+  // axios.all([articles, oaArticles, canArchive])
+  // .then(axios.spread((...responses) => {
+  //   let articlesContents = document.querySelector("#articles"),
+  //   oaArticlesContents = document.querySelector("#articles_oa"),
+  //   oaPercentageContents = document.querySelector("#percent_oa"),
+  //   canArchiveContents = document.querySelector("#can_archive"),
+  //   canArchiveOaPercentageContents = document.querySelector("#can_archive_percent_oa"),
+  //   canArchiveLatestContents = document.querySelector("#can_archive_latest"),
+  //   canArchiveLatestJournalContents = document.querySelector("#can_archive_latest_journal");
+  //
+  //   const articles = responses[0].data,
+  //   oaArticles = responses[1].data,
+  //   oaPercentage = ((oaArticles/articles)*100).toFixed(2),
+  //   canArchive = responses[2].data.hits.total,
+  //   canArchiveAll = responses[2].data.hits.hits,
+  //   canArchiveOaPercentage = ((((oaArticles+canArchive))/articles)*100).toFixed(2),
+  //   canArchiveLatest = responses[2].data.hits.hits[0]._source.title,
+  //   canArchiveLatestJournal = responses[2].data.hits.hits[0]._source.journal;
+  //
+  //   articlesContents.textContent = articles;
+  //   oaArticlesContents.textContent = oaArticles;
+  //   oaPercentageContents.textContent = oaPercentage;
+  //   canArchiveContents.textContent = canArchive;
+  //   canArchiveOaPercentageContents.textContent = canArchiveOaPercentage;
+  //   canArchiveLatestContents.textContent = canArchiveLatest;
+  //   canArchiveLatestJournalContents.textContent = canArchiveLatestJournal;
+  //
+  // })).catch(error => console.error(error));
 };
 
 oareport(org);
