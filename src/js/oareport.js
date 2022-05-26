e9b1a85637b72997d4b44d1dcb8b922a302f259a
sssoz <ouchsophy@gmail.com>
2022-05-26 15:03:09 +0100

    Display mailto link, open article in new tabÉ

--- src/js/oareport.js
+++ src/js/oareport.js
@@ -139,13 +139,13 @@ oareport = function(org) {
           // </article></li>";
           canArchiveListItems += '<tr>\
             <td class="py-4 pl-4 pr-3 text-sm align-top break-words">\
-              <div class="font-medium text-neutral-900"><a href="https://doi.org/' + doi + '" target="_blank">' + title + '</a></div>\
+              <div class="font-medium text-neutral-900"><a href="https://doi.org/' + doi + '" target="_blank" rel="noopener" title="Open article">' + title + '</a></div>\
               <div class="text-neutral-500">' + journal + '</div>\
             </td>\
             <td class="hidden px-3 py-4 text-sm text-neutral-500 align-top break-words sm:table-cell">' + authors + '</td>\
             <td class="hidden whitespace-nowrap px-3 py-4 text-sm text-neutral-500 align-top lg:table-cell">' + pubDate + '</td>\
             <td class="whitespace-nowrap py-4 pl-3 pr-4 text-center align-top text-sm font-medium">\
-              <a href="#" class="hover:text-carnation-500">\
+              <a href="' + canArchiveAAMMailto + '" class="hover:text-carnation-500">\
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail inline-block h-4 duration-500"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>\
               </a>\
             </td>\
