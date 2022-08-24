85df98bf7f4068d9b07adb924b2192d97c7bfb94
sssoz <ouchsophy@gmail.com>
2022-08-24 20:14:03 +0100

    Ensure tabs are a11y-compliant
    See oaworks/Gates#310

--- src/js/ui.js
+++ src/js/ui.js
@@ -11,19 +11,28 @@ tabItems.forEach(function(toggler) {
 
     for (let i = 0; i < tabContent.children.length; i++) {
 
+      // Tabs’ appearance when NOT selected
       tabItems[i].classList.remove("border-neutral-900", "text-neutral-900", "font-bold");
       tabItems[i].classList.add("hover:text-neutral-700", "hover:border-neutral-300", "font-normal");
+      tabItems[i].setAttribute("aria-selected", "false");
+      tabItems[i].setAttribute("tabindex", "-1");
 
+      // Tab not selected, thus hide its content
       tabContent.children[i].classList.remove("hidden");
+      tabContent.children[i].removeAttribute("hidden");
 
       if ("#" + tabContent.children[i].id === tabID) {
         continue;
       }
       tabContent.children[i].classList.add("hidden");
-
+      tabContent.children[i].setAttribute("hidden", "");
     }
+
+    // Tab’s appearance when selected
     e.target.classList.add("border-neutral-900", "text-neutral-900", "font-bold");
     e.target.classList.remove("hover:text-neutral-700", "hover:border-neutral-300", "font-normal");
+    e.target.setAttribute("aria-selected", "true");
+    e.target.setAttribute("tabindex", "0");
   });
 });
 
