3a0051518a77d83ee1ae9118adbde53ef1fa13de
sssoz <ouchsophy@gmail.com>
2023-01-08 14:47:19 +0000

    Lower type size

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function() {
   var valid = undefined;
   var showMessage = function(e) {
     // display thank you text on success
-    var newsletterThanks = "<p class='type-1 fw-900'>Thanks for signing up! We will be in touch.</p>";
+    var newsletterThanks = "<p class='type-4 fw-400'>Thanks for signing up! We will be in touch.</p>";
     document.getElementById("newsletter-inner").innerHTML = newsletterThanks;
   };
 
