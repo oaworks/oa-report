2aff0cf29ed1d94549fcff87351aa1e1465dd9bf
Sophy <sssoz@users.noreply.github.com>
2023-03-22 13:41:57 +0100

    Pass through src/img dir

--- .eleventy.js
+++ .eleventy.js
@@ -68,6 +68,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/fonts/');
   eleventyConfig.addPassthroughCopy('./src/js/');
   eleventyConfig.addPassthroughCopy('./src/css/');
+  eleventyConfig.addPassthroughCopy('./src/img/');
   eleventyConfig.addPassthroughCopy('./src/favicons/');
 
   eleventyConfig.setLiquidOptions({
