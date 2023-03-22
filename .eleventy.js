fc3c986a9198a0b24631cb4329a459746a4c209a
Sophy <sssoz@users.noreply.github.com>
2023-03-22 19:57:18 +0100

    Pass thru media/

--- .eleventy.js
+++ .eleventy.js
@@ -69,6 +69,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/js/');
   eleventyConfig.addPassthroughCopy('./src/css/');
   eleventyConfig.addPassthroughCopy('./src/img/');
+  eleventyConfig.addPassthroughCopy('./src/media/');
   eleventyConfig.addPassthroughCopy('./src/favicons/');
 
   eleventyConfig.setLiquidOptions({
