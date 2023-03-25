a74a9a76744b161baa4a39e8557b38595e6336b0
Sophy <sssoz@users.noreply.github.com>
2023-03-25 12:12:58 +0100

    Drop concepts.score and concept.level
    See oaworks/Gates#484

--- src/js/oareport.js
+++ src/js/oareport.js
@@ -115,7 +115,7 @@ if (hasOrgKey) {
 let report = base + "orgs?q=name:%22" + org + "%22";
 
 // Set default export_includes
-let exportIncludes = "&include=DOI,title,subtitle,publisher,journal,issn,published,published_year,PMCID,volume,issue,authorships.author.display_name,authorships.author.orcid,authorships.institutions.display_name,authorships.institutions.ror,funder.name,funder.award,is_oa,oa_status,journal_oa_type,publisher_license,has_repository_copy,repository_license,repository_version,repository_url,has_oa_locations_embargoed,can_archive,version,concepts.display_name,concepts.level,concepts.score,subject,pmc_has_data_availability_statement,cited_by_count";
+let exportIncludes = "&include=DOI,title,subtitle,publisher,journal,issn,published,published_year,PMCID,volume,issue,authorships.author.display_name,authorships.author.orcid,authorships.institutions.display_name,authorships.institutions.ror,funder.name,funder.award,is_oa,oa_status,journal_oa_type,publisher_license,has_repository_copy,repository_license,repository_version,repository_url,has_oa_locations_embargoed,can_archive,version,concepts.display_name,subject,pmc_has_data_availability_statement,cited_by_count";
 
 // Generate report’s UI for any given date range
 oareport = function(org) {
