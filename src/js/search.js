ea9d1ac33ad9142c245873f30b79d766f426b9c6
Sophy <sssoz@users.noreply.github.com>
2023-03-31 16:49:49 +0200

    style search
    See oaworks/Gates#187

--- src/js/search.js
+++ src/js/search.js
@@ -17,7 +17,7 @@ async function fetchSuggestions(searchTerm) {
         const filteredData = data.filter(item => !item.private);
 
         if (Array.isArray(filteredData) && filteredData.some(item => item.hasOwnProperty('name'))) {
-            suggestionsList.innerHTML = filteredData.map(result => `<li><a href="https://oa.report/${result.objectID}">${result.name}</a></li>`).join('');
+            suggestionsList.innerHTML = filteredData.map(result => `<li class="relative cursor-default select-none p-3 text-neutral-700 hover:font-semibold hover:text-white hover:bg-neutral-900"><a href="https://oa.report/${result.objectID}"><span class="block truncate">${result.name}</span></a></li>`).join('');
             suggestionsList.style.display = 'block';
         } else {
             suggestionsList.innerHTML = '<li>No results! If you think there should be, <a href="mailto:contact@yourdomain.com">get in touch</a>.</li>';
