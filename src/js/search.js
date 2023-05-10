e22d82ee74504eae76bbea64eb66924e50b2c8a9
Sophy <sssoz@users.noreply.github.com>
2023-05-10 19:37:06 +0100

    Harmonise search bar forms & signup forms
    See oaworks/internal-planning#369

--- src/js/search.js
+++ src/js/search.js
@@ -17,10 +17,10 @@ async function fetchSuggestions(searchTerm) {
         const filteredData = data.filter(item => !item.private);
 
         if (Array.isArray(filteredData) && filteredData.some(item => item.hasOwnProperty('name'))) {
-            suggestionsList.innerHTML = filteredData.map(result => `<li class="relative cursor-default select-none p-3 text-neutral-700 hover:font-semibold hover:text-white hover:bg-neutral-900"><a href="/${result.objectID}"><span class="block truncate">${result.name}</span></a></li>`).join('');
+            suggestionsList.innerHTML = filteredData.map(result => `<li class="relative cursor-default border-b select-none text-neutral-700 hover:font-semibold hover:text-white hover:bg-neutral-900"><a href="/${result.objectID}"><span class="p-3 block truncate">${result.name}</span></a></li>`).join('');
             suggestionsList.style.display = 'block';
         } else {
-            suggestionsList.innerHTML = '<li class="relative cursor-default select-none p-3 text-neutral-900">No results! If you think there should be, <a href="mailto:contact@yourdomain.com" class="underline underline-offset-2 decoration-1">get in touch</a>.</li>';
+            suggestionsList.innerHTML = '<li class="relative cursor-default border-b select-none p-3 text-neutral-900">No results! If you think there should be, <a href="mailto:contact@yourdomain.com" class="underline underline-offset-2 decoration-1">get in touch</a>.</li>';
             suggestionsList.style.display = 'block';
         }
     } catch (error) {
