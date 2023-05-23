f77ba9682ec9043b82cd9cb1f024af6f5865b79b
Sophy <sssoz@users.noreply.github.com>
2023-05-23 16:58:53 +0200

    Exclude export_includes

--- src/_data/orgs.js
+++ src/_data/orgs.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 module.exports = async () => {
-  const { data } = await axios.get("https://bg.beta.oa.works/report/orgs?q=*&excludes=aliases,fundref,sheets,analysis,strategy&size=600");
+  const { data } = await axios.get("https://bg.beta.oa.works/report/orgs?q=*&excludes=aliases,fundref,sheets,analysis,strategy,export_includes&size=600");
 
   return data;
 };
