7a04520a08f18f89a7b78a04fe899db0fab07f4d
Sophy <sssoz@users.noreply.github.com>
2023-05-23 16:42:52 +0200

    Decrease reports to 400

--- src/_data/orgs.js
+++ src/_data/orgs.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 module.exports = async () => {
-  const { data } = await axios.get("https://beta.oa.works/report/orgs?q=*&excludes=aliases,fundref,sheets,analysis,strategy&size=600");
+  const { data } = await axios.get("https://beta.oa.works/report/orgs?q=*&excludes=aliases,fundref,sheets,analysis,strategy&size=400");
 
   return data;
 };
