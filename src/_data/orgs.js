b64de2dbeed4af9a2b55f862a1c53d0708aeaecf
Sophy <sssoz@users.noreply.github.com>
2023-05-23 17:02:02 +0200

    Update excludes

--- src/_data/orgs.js
+++ src/_data/orgs.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 module.exports = async () => {
-  const { data } = await axios.get("https://bg.beta.oa.works/report/orgs?q=*&excludes=aliases,fundref,sheets,analysis,strategy,export_includes&size=600");
+  const { data } = await axios.get("https://bg.beta.oa.works/report/orgs?q=*&excludes=aliases,fundref,sheets,analysis,strategy,export_includes,search,allow_tdm,grantid_regex,source&size=600");
 
   return data;
 };
