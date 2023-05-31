66a9765d8fa4164d4f49c4e6155c9a40937183aa
Sophy <sssoz@users.noreply.github.com>
2023-05-31 19:40:26 +0100

    Performance improvements: support WebP images

--- .eleventy.js
+++ .eleventy.js
@@ -79,10 +79,47 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/*.xml");
   eleventyConfig.addPassthroughCopy("./src/*.txt");
 
+  // Add NJK template tag for WebP images (and fallback)
+  // Image is included this way: {% webpImg "images/image.jpg", "Alt text here", "Optional custom class here" %}
+  eleventyConfig.addShortcode("webpImg", function(filename, altText, classes = '', loading = false) {
+    const webpFilename = filename.replace(/\.[^.]+$/, ".webp");
+
+    // Call checkWebPSupport() here to determine WebP support
+    let hasWebPSupport = false;
+    checkWebPSupport(function(supported) {
+      hasWebPSupport = true;
+    });
+
+    const imgClasses = classes ? `class="${classes}"` : '';
+    const loadingAttr = loading ? 'loading="lazy"' : '';
+
+    if (hasWebPSupport) {
+      return `<picture>
+                <source srcset="${webpFilename}" type="image/webp">
+                <img src="${filename}" alt="${altText}" ${imgClasses} ${loadingAttr}>
+              </picture>`;
+    } else {
+      return `<img src="${filename}" alt="${altText}" ${imgClasses} ${loadingAttr}>`;
+    }
+  });
+
+  // Define checkWebPSupport() function 
+  function checkWebPSupport(callback) {
+    const img = new (require('canvas').Image)();
+    img.onerror = function() {
+      callback(false);
+    };
+    img.onload = function() {
+      callback(true);
+    };
+    img.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=';
+  }
+
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true
   });
 
+  // Define output directory when compiling assets
   return {
     dir: {
       input: 'src',
