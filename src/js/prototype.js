be2ed3233779db6c59ad6fee1ead3762273640f2
Sophy <sssoz@users.noreply.github.com>
2023-10-19 21:08:09 +0100

    Add sticky year nav back

--- src/js/prototype.js
+++ src/js/prototype.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
     exportLink.removeAttribute('href');
     exportLink.classList.add('bg-neutral-300', 'cursor-not-allowed');
     // exportLink.classList.remove('bg-carnation-500', 'hover:bg-carnation-300', 'hover:border-carnation-500');
-  }
+  }  
 
   // Toggle data-display style
   let currentToggleHandler = null;  // To keep track of the current event handler
@@ -108,6 +108,18 @@ document.addEventListener("DOMContentLoaded", function() {
     toggleButton.addEventListener('click', currentToggleHandler);
   }
 
+  // Sticky year header — add padding when the year is at the top
+  document.addEventListener("scroll", function() {
+    const nav = document.querySelector("#top_nav");
+    const rect = nav.getBoundingClientRect();
+    
+    // Add 'pb-6' class when the nav is at the top of the viewport
+    if (rect.top <= 0) {
+      nav.classList.add("pb-6");
+    } else {
+      nav.classList.remove("pb-6");
+    }
+  });
 
   // Listen button click of "grants"
   const exportGrantsBtn = document.getElementById('export_grant');
