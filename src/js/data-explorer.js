85dc35ba1572c9d8feb4ce2a980b27683f24ace5
Sophy <sssoz@users.noreply.github.com>
2023-10-27 11:02:18 +0100

    Remove console log

--- src/js/data-explorer.js
+++ src/js/data-explorer.js
@@ -142,7 +142,6 @@ async function displayTableBody(groupByKeyName) {
   }
 
   const postData = createPostData(orgName, groupByKeyName, startYear, endYear);
-  console.log(orgName);
   const responseData = await fetchData(postData);  // Capture the response data
   
   if (responseData) {  // Ensure that data exists before passing it to displayData
