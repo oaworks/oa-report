ae906ece37a78aba020165ce7a37844fb4eb1f9d
Sophy <sssoz@users.noreply.github.com>
2023-12-06 12:11:56 +0000

    Fix strategy btns

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,8 @@ const allTimeBtn = document.getElementById("all-time");
 const reportDateRange = document.getElementById("report-range");
 const reportYear = document.getElementById("report-year");
 
+const strategyTabBtns = document.querySelectorAll(".js_strategy_btn");
+
 // Initialisation
 document.addEventListener("DOMContentLoaded", initialise);
 
@@ -153,6 +155,53 @@ function adjustNavOnScroll() {
   // Call the function immediately to check the initial scroll position
   adjustNavStyle();
 }
+
+/**
+ * Handles click events on tab buttons to switch between different tabs.
+ * This function controls the display of tab content and updates the appearance of tabs based on the selected state.
+ *
+ * @param {Event} event - The click event object.
+ */
+function updateStrategyButtonStyling(event) {
+  // Check if the target is a span and stop propagation if so
+  if (event.target.tagName.toLowerCase() === 'span') {
+    event.stopPropagation();
+    return;
+  }
+
+  const tabBtn = event.currentTarget;
+  const selectedStrategy = tabBtn.getAttribute("aria-controls");
+  const selectedTabContents = document.querySelector(`.js_strategies #${selectedStrategy}`);
+  const otherTabContents = document.querySelectorAll(`.js_strategy:not(#${selectedStrategy})`);
+  const otherTabBtns = document.querySelectorAll(`.js_strategy_btn:not(#strategy_${selectedStrategy})`);
+
+  // When unselected
+  for (let tabContents of otherTabContents) {
+    tabContents.classList.add("hidden");
+    tabContents.setAttribute("hidden", true);
+  }
+
+  for (let unselectedTabBtn of otherTabBtns) {
+    unselectedTabBtn.classList.remove("bg-carnation-300", "font-semibold");
+    unselectedTabBtn.classList.add("hover:text-neutral-700", "hover:border-neutral-300", "font-normal");
+    unselectedTabBtn.setAttribute("aria-selected", "false");
+    unselectedTabBtn.setAttribute("tabindex", "-1");
+  }
+
+  // When selected
+  selectedTabContents.classList.remove("hidden");
+  selectedTabContents.removeAttribute("hidden");
+
+  tabBtn.classList.add("bg-carnation-300", "font-semibold");
+  tabBtn.classList.remove("hover:text-neutral-700", "hover:border-neutral-300", "font-normal");
+  tabBtn.setAttribute("aria-selected", "true");
+  tabBtn.setAttribute("tabindex", "0");
+}
+
+// Attach event listeners to strategy buttons
+strategyTabBtns.forEach((tabBtn) => {
+  tabBtn.addEventListener("click", updateStrategyButtonStyling);
+});
 
 // Start the initialisation process
 initialise();
