bd0874b553ab457dcb8688825ce2c53540973a0e
Sophy <sssoz@users.noreply.github.com>
2024-01-09 22:12:35 +0000

    Add labels

--- src/js/constants.js
+++ src/js/constants.js
@@ -99,7 +99,10 @@ export const EXPLORE_TYPES = {
 export const EXPLORE_FILTERS = {
   "is_paper": "All articles",
   "is_preprint": "Preprints",
+  "is_covered_by_policy": "Covered by policy",
+  "with_grant_id": "With grant ID",
   "is_found_only_by_external_sources": "Found only by external sources",
+  "is_found_only_by_oareport": "Found only by OA.Report",
   "is_found_by_all_sources": "Found by all sources",
 };
 
