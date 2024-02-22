c6b99a7381824d54b42cddcd610c2ae22921d39e
Sophy <sssoz@users.noreply.github.com>
2024-02-22 13:52:01 +0000

    Use white tooltips
    See oaworks/Gates#662

--- src/js/utils.js
+++ src/js/utils.js
@@ -653,6 +653,7 @@ export function setupTooltip(element, tooltipContent, key) {
     interactive: true,
     placement: 'bottom',
     appendTo: document.body,
+    theme: 'table-tooltip',
   });
 
   // Access tooltip instance and its ID; use it for aria-controls attribute
