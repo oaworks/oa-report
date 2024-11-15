eb3718b8b1e27eb544e33ef1ebb24f39dc675833
Sophy <sssoz@users.noreply.github.com>
2024-11-15 16:40:22 +0000

    Refine colour palette
    See oaworks/discussion#3264

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -26,19 +26,19 @@ module.exports = {
       current: 'currentColor',
       'white': '#fff',
       'carnation': {
-        100: '#f4f0f0',
-        200: '#fecfcf',
-        300: '#ffaeac',
-        400: '#ff8b89',
-        500: '#ff6666',
-        600: '#e65353',
-        700: '#cc4040',
-        800: '#b32e2e',
-        900: '#992222',
+        100: '#F4F0F0', // Very light, subtle background
+        200: '#F9CFCF',
+        300: '#F79E9E',
+        400: '#F66E6E', // Accent option
+        500: '#E64E4E', // Primary action color
+        600: '#CC4242', // Darker variant for hover
+        700: '#B23636',
+        800: '#992C2C',
+        900: '#802323', // Deepest shade for text or strong accents
       },
       'neutral': {
         50:  '#F9F9FA',
-        100: '#F0F0F1',
+        100: '#F7F7F8',
         200: '#E2E3E5',
         300: '#C8C9CC',
         400: '#ABADB0',
@@ -48,47 +48,30 @@ module.exports = {
         800: '#434446',
         900: '#2E2E2F',
       },
-      // Complementary colors
-      'navy-blue': {
-        100: '#e0e6ea',
-        200: '#99b1c2',
-        300: '#002237',
-        400: '#001d30',
-        500: '#001424',
-        600: '#000c16',
+      'blue': {
+        100: '#E6F2FF',
+        200: '#D0E7FF',
+        300: '#A9D0FF',
+        400: '#719ECE', // Accent option
+        500: '#5A7FCC',
+        600: '#4A6AB2',
       },
-      'light-blue': {
-        100: '#e6f2ff',
-        200: '#d0e7ff',
-        300: '#b8d4ff',
-        400: '#90baff',
-        500: '#7099e0',
-        600: '#5a7fcc',
-      },
-      'coral-peach': {
-        100: '#ffe8e1',
-        200: '#ffd6ca',
-        300: '#ffb39d',
-        400: '#ff937b',
-        500: '#e07668',
-        600: '#cc614f',
-      },
-      'forest-green': {
-        100: '#e7f2eb',
-        200: '#cce1d4',
-        300: '#59836a',
-        400: '#3d6a51',
-        500: '#2e5441',
-        600: '#243f33',
+      'green': {
+        100: '#E7F2EB',
+        200: '#CCE1D4',
+        300: '#59836A', // Accent option
+        400: '#3D6A51',
+        500: '#2E5441',
+        600: '#243F33',
       },
       'yellow': {
-        100: '#fff2da',
-        200: '#ffdeab',
-        300: '#ffc978',
-        400: '#ffb060',
-        500: '#e69d55',
-        600: '#cc8b4c',
-      }
+        100: '#FFF7DA',
+        200: '#FFE8AB',
+        300: '#FFD97A',
+        400: '#FFBF60', // Accent option
+        500: '#E69D55',
+        600: '#CC8B4C',
+      },
     },
   },
 }
