b15ec063cdbb8bc3e87f9c87bbd49779f1339d18
Sophy <sssoz@users.noreply.github.com>
2026-04-29 11:26:14 +0200

    Improve copy and include incident status link
    See oaworks/discussion#3828

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     "base_url": process.env.ELEVENTY_BASE_URL,
-    "banner": "We are experiencing temporary issues with loss of OA.Report data. We are investigating and will resolve it as quickly as possible.",
+    "banner": "We’re currently experiencing an issue affecting OA.Report data. We’re investigating and working to restore normal service as quickly as possible.<br><a href='https://about.oa.report/help/oa.report-2026-04-29-incident-status-updates' target='_blank' rel='noopener'>Read incident updates</a>.",
     "name": "OA.Report",
     "tagline": "Unlock your institution’s research with <span class='font-semibold'>OA.Report</span>’s open insights and one-click actions",
     "description": "<span class='highlight'>Discover</span> your institution's publications • <span class='highlight'>Analyze</span> how open your research is and OApolicy compliance • <span class='highlight'>Unlock</span> research and your investment’s full impact",
