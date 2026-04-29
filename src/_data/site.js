468dc757562f62798ccf1b9dc61214d13a27f497
Sophy <sssoz@users.noreply.github.com>
2026-04-29 10:04:53 +0200

    Display data loss notice
    see oaworks/discussion#3827

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     "base_url": process.env.ELEVENTY_BASE_URL,
+    "banner": "We are experiencing temporary issues with loss of OA.Report data. We are investigating and will resolve it as quickly as possible.",
     "name": "OA.Report",
     "tagline": "Unlock your institution’s research with <span class='font-semibold'>OA.Report</span>’s open insights and one-click actions",
     "description": "<span class='highlight'>Discover</span> your institution's publications • <span class='highlight'>Analyze</span> how open your research is and OApolicy compliance • <span class='highlight'>Unlock</span> research and your investment’s full impact",
