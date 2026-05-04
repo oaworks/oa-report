cc6ccd6e7a271937aab8c3c1a35cab9e9de867f9
Sophy <sssoz@users.noreply.github.com>
2026-05-04 10:25:12 +0100

    Revert "Temp. Hardcode api.oa.works/reports/orgs"
    This reverts commit ce90692b6ea88cd24b78fd84f7c87ae0d7663739.

--- src/js/insights-and-actions.js
+++ src/js/insights-and-actions.js
@@ -29,7 +29,7 @@ const fetchCountQuery = (url) => {
 // =================================================
 
 // Set report org index URL’s base path
-export const orgApiUrl = `https://api.oa.works/report/orgs?q=objectID:%22${org}%22`;
+export const orgApiUrl = `${API_BASE_URL}orgs?q=objectID:%22${org}%22`;
 
 // Fetch and store organisational data in a constant
 export const orgDataPromise = axios.get(orgApiUrl);
